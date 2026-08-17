9b8f9d3c4d7c7401550682d3b8902c415125408d
Eli Lab <43141668+ELIASADAMS@users.noreply.github.com>
2026-08-18 02:18:52 +0900

    chore: remove obsolete broken build script

--- build opt.py
+++
@@ -1,8 +0,0 @@
-pyinstaller - -noconfirm - -onefile - -noconsole - -windowed - -icon
-"E:\VIDEO\eli_lab-pipeline-logo_Монтажная-область-1.ico" - -name
-"eli_lab multimedia framework" - -disable - windowed - traceback - -add - data
-"D:\Ierarchy\ELI_LAB STUDIO\8. Coding\eli_lab-multimedia-framework\eli_lab mf\advanced_template_system.py;." - -add - data
-"D:\Ierarchy\ELI_LAB STUDIO\8. Coding\eli_lab-multimedia-framework\eli_lab mf\custom_file_renaming.py;." - -add - data
-"D:\Ierarchy\ELI_LAB STUDIO\8. Coding\eli_lab-multimedia-framework\eli_lab mf\project_metadata_integration.py;." - -add - data
-"D:\Ierarchy\ELI_LAB STUDIO\8. Coding\eli_lab-multimedia-framework\eli_lab mf\setup.py;." - -add - data
-"D:\Ierarchy\ELI_LAB STUDIO\8. Coding\eli_lab-multimedia-framework\eli_lab mf\texture_batch_optimising_tool.py;."  "D:\Ierarchy\ELI_LAB STUDIO\8. Coding\eli_lab-multimedia-framework\eli_lab mf\init.py"
