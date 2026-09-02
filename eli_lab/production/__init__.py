90fc6d725f9bacb0c09acb70d85ec7c01686027d
eli_lab <43141668+ilyaminineli@users.noreply.github.com>
2026-09-02 17:20:18 +0300

    Export production intelligence services

--- eli_lab/production/__init__.py
+++ eli_lab/production/__init__.py
@@ -1,15 +1,26 @@
-"""Production intelligence for eli_lab projects."""
+"""Production intelligence services for eli_lab."""
 
+from .blender_inspection import BlenderReference, find_blender, inspect_blend, inspect_project
 from .dependencies import Dependency, dependencies_for, discover_dependencies
 from .intelligence import TextureInfo, TextureSet, discover_texture_sets, group_texture_set, inspect_texture
+from .materials import MaterialRecord, apply_texture_relocation, canonical_texture_directory, discover_material_records, plan_texture_relocation
 
 __all__ = [
+    "BlenderReference",
     "Dependency",
+    "MaterialRecord",
     "TextureInfo",
     "TextureSet",
+    "apply_texture_relocation",
+    "canonical_texture_directory",
     "dependencies_for",
     "discover_dependencies",
+    "discover_material_records",
     "discover_texture_sets",
+    "find_blender",
     "group_texture_set",
+    "inspect_blend",
+    "inspect_project",
     "inspect_texture",
+    "plan_texture_relocation",
 ]
